6e47184e254625378313ee174d70a5962d71434b
Tuyen <tuyen.cntt.k13a@gmail.com>
2022-08-17 16:19:50 +0700

    First commit

---
+++ app/src/main/java/com/vietko/mvvm/data/model/User.java
@@ -0,0 +1,12 @@
+package com.vietko.mvvm.data.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class User {
+
+    @SerializedName("id")
+    private Integer id;
+
+    @SerializedName("name")
+    private String name;
+}
---
+++ app/src/main/java/com/vietko/mvvm/view/MainActivity.java
@@ -0,0 +1,16 @@
+package com.vietko.mvvm.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.vietko.mvvm.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
